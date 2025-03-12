2b964e70c1ee7e799c966e9897dcc77a0b5c7f1f
Ratchanon Panmas <28267945+ratchanonp@users.noreply.github.com>
2025-03-12 09:57:24 +0700

    🔧 refactor(zoomScheduler): Migrate to new license renewal scheduler
    Migrates the Zoom license scheduler to use the new `LicenseRenewalFactory`
    instead of the deprecated `LicenseSchedulerFactory`. This change ensures
    that the Zoom license borrowing process is handled by the updated and
    more robust license renewal system.

--- functions/src/schedulers/zoomScheduler.ts
+++ functions/src/schedulers/zoomScheduler.ts
@@ -1,5 +1,5 @@
 import {ProgramLicenseID} from "../types/license";
-import {LicenseSchedulerFactory, SchedulerConfig} from "../utils/licenseSchedulerFactory";
+import {LicenseRenewalFactory, SchedulerConfig} from "../utils/licenseSchedulerFactory";
 
 /**
  * Schedules a cron job to borrow a Zoom license every 4 months on the 1st day at midnight.
@@ -10,5 +10,6 @@ const config: SchedulerConfig = {
   cronExpression: "0 0 1 */4 *",
 };
 
-const scheduler = LicenseSchedulerFactory.getScheduler(config);
-export const borrowZoom = scheduler.createScheduler();
+const scheduler = LicenseRenewalFactory.createRenewalScheduler(config);
+
+export const borrowZoom = scheduler.createRenewalTask();
